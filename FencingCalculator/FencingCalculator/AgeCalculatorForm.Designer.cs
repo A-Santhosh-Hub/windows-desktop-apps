90bb9ff6135de24747b70403eac4da8bfd0200d8
SANTHOSH A <arussanthosh@gmail.com>
2026-04-02 23:01:27 +0530

    Add files via upload

---
+++ FencingCalculator/FencingCalculator/AgeCalculatorForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FencingCalculator
+{
+    partial class AgeCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDOB = new System.Windows.Forms.DateTimePicker();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAgeResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(301, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Date of Birth:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // dtpDOB
+            // 
+            this.dtpDOB.CalendarForeColor = System.Drawing.Color.Cyan;
+            this.dtpDOB.CalendarTitleForeColor = System.Drawing.Color.Fuchsia;
+            this.dtpDOB.CalendarTrailingForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.dtpDOB.Font = new System.Drawing.Font("ROG Fonts", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDOB.Location = new System.Drawing.Point(263, 119);
+            this.dtpDOB.Name = "dtpDOB";
+            this.dtpDOB.Size = new System.Drawing.Size(272, 27);
+            this.dtpDOB.TabIndex = 5;
+            this.dtpDOB.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(351, 168);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate Age";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAgeResult
+            // 
+            this.lblAgeResult.AutoSize = true;
+            this.lblAgeResult.Location = new System.Drawing.Point(313, 222);
+            this.lblAgeResult.Name = "lblAgeResult";
+            this.lblAgeResult.Size = new System.Drawing.Size(54, 13);
+            this.lblAgeResult.TabIndex = 7;
+            this.lblAgeResult.Text = "Your Age:";
+            this.lblAgeResult.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAgeResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.dtpDOB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDOB;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAgeResult;
+    }
+}
+
---
+++ FencingCalculator/FencingCalculator/AgeCalculatorForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FencingCalculator
+{
+    public partial class AgeCalculatorForm : Form
+    {
+        public AgeCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            DateTime birthDate = dtpDOB.Value;
+            DateTime today = DateTime.Today;
+
+            int years = today.Year - birthDate.Year;
+            int months = today.Month - birthDate.Month;
+            int days = today.Day - birthDate.Day;
+
+            if (days < 0)
+            {
+                months--;
+                days += DateTime.DaysInMonth(today.Year, today.Month - 1);
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+            DateTime nextBirthday = birthDate.AddYears(years + 1);
+
+            int daysLeft = (nextBirthday - today).Days;
+
+            lblAgeResult.Text =
+                "Your Age: " + years + " Years, " +
+                months + " Months, " +
+                days + " Days\n" +
+                "Next Birthday in: " + daysLeft + " Days 🎂";
+        }
+    }
+}
